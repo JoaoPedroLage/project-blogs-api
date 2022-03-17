adfb846a8a44fe8902d5806e3d6ba25c94498c92
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-03-17 20:19:51 -0300

    feat: set routes (login, user, post,  category) to use

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
+const { userRouter, loginRouter, postRouter, categoryRouter } = require('./routes');
+
+app.use(express.json());
+
+app.use('/login', loginRouter);
+app.use('/user', userRouter);
+app.use('/post', postRouter);
+app.use('/categories', categoryRouter);
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
