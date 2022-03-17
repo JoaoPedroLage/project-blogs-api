df757707d4a3c411a4a4454644c60c64d2f083db
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-03-17 20:57:54 -0300

    feat: create a Joi validation to validate the request to update a post

---
+++ schemas/validationUpdatedPost.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  title: Joi.string()
+  .required()
+  .messages({
+    'string.base': '400|"title" must be a string',
+    'any.required': '400|"title" is required',
+  }),
+  content: Joi.string()
+  .required()
+  .messages({
+    'string.base': '400|"content" must be a string',
+    'any.required': '400|"content" is required',
+  }),
+  categoryIds: Joi.array()
+  .forbidden()
+  .messages({
+    'any.unknown': '400|Categories cannot be edited',
+  }),
+});
